ca2077f50bea96698cf6d400593995fcaa03047a
reyear <reyearocean@qq.com>
2026-02-28 15:38:27 +0800

    nixos-rebuild: 2026-02-28 15:38:27

--- hosts/reyear-nixos/home/editors.nix
+++ hosts/reyear-nixos/home/editors.nix
@@ -3,20 +3,22 @@
 {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      ms-python.python
-      rust-lang.rust-analyzer
-      golang.go
-      dbaeumer.vscode-eslint
-      esbenp.prettier-vscode
-    ];
-    userSettings = {
-      "editor.formatOnSave" = true;
-      "editor.tabSize" = 2;
-      "files.trimTrailingWhitespace" = true;
-      "git.autofetch" = true;
-      "terminal.integrated.defaultProfile.linux" = "zsh";
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        ms-python.python
+        rust-lang.rust-analyzer
+        golang.go
+        dbaeumer.vscode-eslint
+        esbenp.prettier-vscode
+      ];
+      userSettings = {
+        "editor.formatOnSave" = true;
+        "editor.tabSize" = 2;
+        "files.trimTrailingWhitespace" = true;
+        "git.autofetch" = true;
+        "terminal.integrated.defaultProfile.linux" = "zsh";
+      };
     };
   };
 }
