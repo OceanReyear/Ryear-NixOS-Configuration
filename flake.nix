df98892d7f49178b6368f956c78e15c557906dc5
reyear <reyearocean@qq.com>
2026-02-28 11:25:50 +0800

    nixos-rebuild: 2026-02-28 11:25:50

--- flake.nix
+++ flake.nix
@@ -1,48 +1,32 @@
 {
-  description = "Ryear's NixOS Configuration with Flakes and Home Manager";
+  description = "Ryear's NixOS Configuration with Flakes";
 
   inputs = {
+    # NixOS 官方源，锁定 25.11 分支
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    
+    # 或者使用 unstable 获取最新软件
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    # Home Manager（先注释，后续启用）
+    # home-manager = {
+    #   url = "github:nix-community/home-manager/release-25.11";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-      specialArgs = { inherit inputs pkgs-unstable; };
     in
     {
+      # NixOS 系统配置
       nixosConfigurations.reyear-nixos = nixpkgs.lib.nixosSystem {
-        inherit system specialArgs;
+        inherit system;
+        specialArgs = { inherit inputs; };
         modules = [
-          # 使用简单的相对路径（已验证正确）
           ./hosts/reyear-nixos/configuration.nix
-          ./modules/btrfs.nix
-          ./modules/boot.nix
-          ./modules/desktop.nix
-          ./modules/network.nix
-          ./modules/nix.nix
-          ./modules/users.nix
-          ./modules/security.nix
-          ./modules/snapper.nix
-          ./modules/fonts.nix
-          
-          # Home Manager 集成
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = specialArgs;
-              users.reyear = import ./home/reyear.nix;
-            };
-          }
         ];
       };
     };
